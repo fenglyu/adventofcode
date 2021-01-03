836d847adcabf528b6335e156be4e3f566bc4bd5
Feng Lyu <flv@blizzard.com>
2021-01-04 04:11:40 +0800

    init module utils

---
+++ 2020/09/01.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/fenglyu/adventofcode/util"
+)
+
+func main() {
+
+	report := util.parseBasedOnEachLine()
+	fmt.Println(report)
+}
---
+++ util/input.go
@@ -0,0 +1,78 @@
+package util
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func parseBasedOnEmptyLine() []string {
+	fN := flag.String("file", "input", "File name")
+	flag.Parse()
+
+	file, err := os.Open(*fN)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	report := make([]string, 0)
+
+	scanner := bufio.NewScanner(file)
+	onComma := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+
+		for i := 0; i < len(data)-1; i++ {
+			if data[i] == '\n' && data[i+1] == '\n' {
+				return i + 1, data[0:i], nil
+			}
+		}
+
+		if atEOF {
+			return len(data), data[0:len(data)], nil
+		}
+		// Request more data
+		return 0, nil, nil
+	}
+
+	scanner.Split(onComma)
+
+	for scanner.Scan() {
+		//fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		line := scanner.Text()
+		report = append(report, line)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	return report
+}
+
+func parseBasedOnEachLine() []string {
+	fN := flag.String("file", "input", "File name")
+	flag.Parse()
+
+	file, err := os.Open(*fN)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	report := make([]string, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		//fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		line := scanner.Text()
+		report = append(report, line)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	return report
+}
