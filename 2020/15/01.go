02fff9843d0a2e7c420e6fe6e6eb18a5154c775c
Feng Lyu <flv@blizzard.com>
2021-01-21 10:20:45 +0800

    init 15

--- 2020/15/01.go
+++ 2020/15/01.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/fenglyu/adventofcode/util"
 )
@@ -9,5 +11,20 @@ import (
 func main() {
 
 	report := util.ParseBasedOnEachLine()
-	fmt.Println(report)
+	//fmt.Println(report)
+	turns := make([]int, 0)
+
+	for _, v := range strings.Split(report[0], ",") {
+		val, _ := strconv.Atoi(v)
+		turns = append(turns, val)
+	}
+	fmt.Println(turns)
+
+	count, limit := 0, 2020
+
+	i := 0
+	freq := make(map[int]int)
+	for count < limit {
+		
+	}
 }
