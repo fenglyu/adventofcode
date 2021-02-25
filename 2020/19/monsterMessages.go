6b836532d8293ade02611c1d8c5d47cefd60b1b1
Feng Lyu <flv@blizzard.com>
2021-02-25 08:41:42 +0800

    update

--- 2020/19/monsterMessages.go
+++ 2020/19/monsterMessages.go
@@ -2,12 +2,46 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fenglyu/adventofcode/util"
+	//"github.com/hashicorp/terraform/dag"
 )
 
 func main() {
 
-	report := util.ParseBasedOnEachLine()
-	fmt.Println(report)
+	report := util.ParseBasedOnEmptyLine()
+	fmt.Println(report, len(report))
+
+	for _, v := range strings.Split(report[0], "\n") {
+		r := strings.Split(v, ": ")
+		idx, res := r[0], r[1]
+		if strings.Contains(res, "|") {
+			mapp[idx] = newRule(res)
+		} else if strings.Contains(res, "\"") {
+			mapp[idx] = strings.Trim(res, "\"")
+		}
+	}
+}
+
+type rule struct {
+	a, b []string
+}
+
+func (r *rule) String() string {
+	return fmt.Sprintf("%v | %v", r.a, r.b)
+}
+
+func newRule(res string) *rule {
+	r := strings.Split(res, " | ")
+	a, b := strings.Split(r[0], " "), strings.Split(r[1], " ")
+	return &rule{a: a, b: b}
+}
+
+//var mapp map[int]*rule
+var mapp map[string]interface{}
+
+func validate(target []byte, idx int) bool {
+
+	return false
 }
