1a02ba106f299416b33a308aed01996ccf01034a
Feng Lyu <feng_lyu@apple.com>
2021-12-15 14:54:00 +0800

    add in-place files

--- 2021/15/main.go
+++ 2021/15/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/fenglyu/adventofcode/util"
 )
 
 func main() {
 	report := util.ParseBasedOnEachLine()
-	l := strings.Split(report[0], ",")
-	fmt.Println(l)
+	fmt.Println(report)
 }
---
+++ util/input_test.go
@@ -0,0 +1,9 @@
+package util
+
+import "testing"
+
+func TestParseBasedOnEmptyLine(t *testing.T) {}
+
+func TestParseBasedOnEachLine(t *testing.T) {}
+
+func TestParseIntobyteArray(t *testing.T) {}
