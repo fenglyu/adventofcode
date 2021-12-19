e131e9706d0e4741859c9e9881f7c1575e6d21fa
Feng Lyu <feng_lyu@apple.com>
2021-12-19 22:59:51 +0800

    finish day 7

--- util/tools.go
+++ util/tools.go
@@ -4,3 +4,30 @@ package util
 func Mod(a, b int) int {
 	return (a%b + b) % b
 }
+
+func Max(arr []int) int {
+	max := arr[0]
+	for _, v := range arr {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func Min(arr []int) int {
+	min := arr[0]
+	for _, v := range arr {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func Abs(v int) int {
+	if v >= 0 {
+		return v
+	}
+	return -v
+}
