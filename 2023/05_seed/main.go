59d0223b6fc693123de4522a0dde2995ff22df0b
Feng Lyu <aragon1130@gmail.com>
2023-12-09 23:40:06 +0800

    finally have time to complete 05

--- 2023/05_seed/main.go
+++ 2023/05_seed/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/fenglyu/adventofcode/util"
 )
@@ -17,11 +20,7 @@ type conditionActionPair struct {
 	action    actionFunc
 }
 
-type pieceFuncChain struct {
-	chains []chainFunc
-}
-
-func piecewiseFunction(x int, pairs []conditionActionPair) chainFunc {
+func piecewiseFunction(pairs []conditionActionPair) chainFunc {
 	return func(x int) int {
 		for _, pair := range pairs {
 			if pair.condition(x) {
@@ -35,18 +34,23 @@ func piecewiseFunction(x int, pairs []conditionActionPair) chainFunc {
 func genFunc(arr [][]int) []conditionActionPair {
 	pairs := make([]conditionActionPair, len(arr))
 	for i := 0; i < len(arr); i++ {
+		// Capture the current value of i
+		i := i
 		pairs[i] = conditionActionPair{
-			condition: func(x int) bool { return x >= arr[i][1] && x < arr[i][1]+arr[i][2] },
-			action:    func(x int) int { return arr[i][0] + (x - arr[i][1]) },
+			condition: func(x int) bool {
+				return x >= arr[i][1] && x < arr[i][1]+arr[i][2]
+			},
+			action: func(x int) int { return arr[i][0] + (x - arr[i][1]) },
 		}
 	}
 	return pairs
 }
 
-func locatePostion(seed int, pieceChains []chainFunc) int {
+func locatePostion(seed int, pf [][]conditionActionPair) int {
 	loc := seed
-	for _, pieceFunc := range pieceChains {
-		loc = pieceFunc(loc)
+	for _, v := range pf {
+		pfunc := piecewiseFunction(v)
+		loc = pfunc(loc)
 	}
 	return loc
 }
@@ -82,14 +86,81 @@ func main() {
 	//fmt.Println(len(raw), raw[0])
 
 	seeds := head2Arr(raw[0])
-	fmt.Println("seeds: ", seeds)
-	pwfChains := make([]chainFunc, len(raw)-1)
+	//fmt.Println("seeds: ", seeds)
+	//	pwfChains := make([]chainFunc, len(raw)-1)
+	capArr := make([][]conditionActionPair, 0)
 	for _, v := range raw[1:] {
 		categories := strings.Split(v, ":")
-		hdr, val := strings.TrimSpace(categories[0]), strings.TrimSpace(categories[1])
-		fmt.Println(hdr)
-		fmt.Println(map2Arr(val))
-		piecewiseFunc := piecewiseFunction(x, genFunc(map2Arr(val)))
-
+		_, val := strings.TrimSpace(categories[0]), strings.TrimSpace(categories[1])
+		//fmt.Println(hdr)
+		//fmt.Println(map2Arr(val))
+		//piecewiseFunc := piecewiseFunction(x, genFunc(map2Arr(val)))
+		capArr = append(capArr, genFunc(map2Arr(val)))
 	}
+
+	fmt.Println("problem 1:", lowestLocNum(seeds, capArr))
+
+	//min := math.MaxInt32
+	/*
+		arr := make([]int, 0)
+		for i := 0; i < len(seeds); i += 2 {
+			//sArr := genArr(seeds[i], seeds[i+1])
+			//n := lowestLocNum(sArr, capArr)
+			arr = append(arr, seeds[i])
+		}
+		fmt.Println(arr)
+		fmt.Println("problem 2:", lowestLocNum(arr, capArr))
+	*/
+
+	min := math.MaxInt32
+	var wg sync.WaitGroup
+	resCh := make(chan int, len(seeds)/2)
+	startTime := time.Now()
+	for i := 0; i < len(seeds); i += 2 {
+		sArr := genArr(seeds[i], seeds[i+1])
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			n := lowestLocNum(sArr, capArr)
+			resCh <- n
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(resCh)
+	}()
+
+	for v := range resCh {
+		if v < min {
+			min = v
+		}
+	}
+	endTime := time.Now()
+	duration := endTime.Sub(startTime)
+
+	fmt.Println("problem 2: ", min)
+	fmt.Println("Execution time: ", duration)
+	/*
+	   problem 2:  79004094
+	   Execution time:  2m28.753255334s
+	*/
+}
+
+func genArr(first, count int) []int {
+	res := make([]int, count)
+	for i := 0; i < count; i++ {
+		res[i] = first + i
+	}
+	return res
+}
+
+func lowestLocNum(seeds []int, capArr [][]conditionActionPair) int {
+	min := math.MaxInt32
+	for _, s := range seeds {
+		r := locatePostion(s, capArr)
+		if r < min {
+			min = r
+		}
+	}
+	return min
 }
